839d5c53e900240ddae0b7535edd659a87bb9497
DOUBLE-A-V <lksndrmzlv@gmail.com>
2025-05-26 23:46:35 +0700

    health and effects base added not finalized

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,13 +10,87 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float jumpForce;
-
+    
     private bool pressedJump = false;
     public Vector2 movingSpeed;
     public bool controlsLocked = false;
     private bool isGrounded = false;
     private bool right = true;
     public InvManager.InventoryManager Inventory = new  InvManager.InventoryManager();
+    
+    public float health = 5;
+
+    private const int DAMAGE_NORMAL = 0;
+    
+    private const int EFFECT_DEFENSE = 0;
+    
+    class Effect
+    {
+        public int type;
+        public int strength;
+        public List<int> parameters;
+        public Effect(int effectType_, int strength_)
+        {
+            this.type = effectType_;
+            this.strength = strength_;
+        }
+    }
+
+    class Effects
+    {
+        public List<Effect> effects = new List<Effect>();
+
+        public void addEffect(Effect effect)
+        {
+            foreach (Effect e in this.effects)
+            {
+                if (e.type == effect.type)
+                {
+                    e.strength += effect.strength;
+                    return;
+                }
+            }
+            this.effects.Add(effect);
+        }
+
+        public Effect addEffect(int type, int strength)
+        {
+            Effect effect = new Effect(type, strength);
+            this.addEffect(effect);
+            return effect;
+        }
+
+        public void clear()
+        {
+            this.effects.Clear();
+        }
+
+        public void removeEffect(Effect effect)
+        {
+            foreach (Effect e in this.effects)
+            {
+                if (e.type == effect.type)
+                {
+                    this.effects.Remove(e);
+                    return;
+                }
+            }
+        }
+    }
+
+    Effects effects = new Effects();
+    
+    void updateEffects()
+    {
+        effects.clear();
+        foreach (InvManager.Item item in Inventory.items)
+        {
+            if (item.name == "shield")
+            {
+                effects.addEffect(EFFECT_DEFENSE, 15);
+            }
+        }
+    }
     void Start()
     {
         new Item("null");
@@ -25,6 +99,35 @@ public class Player : MonoBehaviour
         Inventory.printInventory();
         Inventory.removeItem("ultratest");
         Inventory.printInventory();
+    }
+
+    public float affectDamage(int type, int amount)
+    {
+        switch (type)
+        {
+            case DAMAGE_NORMAL:
+                foreach (Effect effect in effects.effects)
+                {
+                    switch (effect.type)
+                    {
+                        case EFFECT_DEFENSE:
+                            amount = (effect.strength / 100) * amount;
+                            break;
+                    }
+                }
+
+                break;
+        }
+        return amount;
+    }
+    void doDamage(int damageType, int amount)
+    {
+        float finalAmount = affectDamage(damageType, amount);
+        changeHealth(finalAmount);
+    }
+    void changeHealth(float amount)
+    {
+        health += amount;
     }
 
     void jump()
